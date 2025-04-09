9b0289c787a985f9c9dc8ae80d2628353fdb5307
awm-achi <achi@smartshelf.com>
2025-04-08 18:53:16 -0700

    helm ls and snacks

--- init.lua
+++ init.lua
@@ -164,8 +164,8 @@ vim.opt.showtabline = 2
 vim.opt.foldmethod = 'expr'
 vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
 vim.opt.foldlevel = 99
-vim.opt.foldlevelstart = 3
-vim.opt.foldnestmax = 4
+-- vim.opt.foldlevelstart = 3
+-- vim.opt.foldnestmax = 4
 vim.opt.foldcolumn = '0'
 vim.opt.foldtext = ''
 
@@ -715,6 +715,7 @@ require('lazy').setup({
             },
           },
         },
+        helm_ls = {},
       }
 
       -- Ensure the servers and tools above are installed
