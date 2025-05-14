114d81446f52ec14ae74c2eeb059fcaa2307421c
awm-achi <austinchi2@Yahoo.com>
2025-05-13 18:28:16 -0700

    migrate dashboard to snacks

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -1,3 +1,13 @@
+-- utility to read header from file
+local function read_header(path)
+  local lines = {}
+  path = vim.fs.normalize(path)
+  for line in io.lines(path) do
+    table.insert(lines, line)
+  end
+  return table.concat(lines, '\n')
+end
+
 vim.api.nvim_create_autocmd('User', {
   pattern = 'MiniFilesActionRename',
   callback = function(event)
@@ -8,24 +18,67 @@ vim.api.nvim_create_autocmd('User', {
 vim.keymap.set('n', '<leader>gb', function()
   Snacks.gitbrowse()
 end, { desc = '[G]it [B]rowse' })
+
 vim.keymap.set('n', '<leader>lg', function()
   Snacks.lazygit()
 end, { desc = '[L]azy [G]it' })
+
 return {
   'folke/snacks.nvim',
   priority = 1000,
   lazy = false,
   opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
     animate = { enabled = true },
     bigfile = { enabled = true },
+    dashboard = { -- Dashboard configuration should be directly here
+      enabled = true,
+      preset = {
+        header = read_header '~/.anya',
+        keys = {
+          {
+            icon = ' ',
+            key = 'e',
+            desc = 'New file',
+            action = ':ene | startinsert',
+          },
+          {
+            icon = ' ',
+            key = 'r',
+            desc = 'Recent',
+            action = function()
+              require('telescope.builtin').oldfiles()
+            end,
+          },
+          {
+            icon = ' ',
+            key = 's',
+            desc = 'Settings',
+            action = ':e ~/.config/nvim/init.lua | pwd',
+          },
+          {
+            icon = '󰒲 ',
+            key = 'l',
+            desc = 'Lazy',
+            action = ':Lazy',
+          },
+          {
+            icon = ' ',
+            key = 'q',
+            desc = 'Quit NVIM',
+            action = ':qa',
+          },
+        },
+        sections = {
+          { header = 'header' },
+          { keys = 'keys' },
+          { section = 'startup' },
+        },
+      },
+    },
     debug = { enabled = true },
     gitbrowse = { enabled = true },
     indent = { enabled = true },
     input = { enabled = true },
-    lazygit = { enabled = true },
     notify = { enabled = true },
     notifier = { enabled = true },
     quickfile = { enabled = true },
